02487143ae6212118699d5fad3b392e265a612a8
Alexandr-Dolgov <samara-fv@yandex.ru>
2015-04-22 03:09:20 +0400

    Изменил тему приложения: убрал заголовк, чтобы не съедалось лишнее экранное пространство.

--- app/src/main/java/com/dolgov/accountancy/MainActivity.java
+++ app/src/main/java/com/dolgov/accountancy/MainActivity.java
@@ -1,6 +1,6 @@
 package com.dolgov.accountancy;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,7 +9,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     TextView date;
     Button prev;
