632d84e2904b9204b1d56fb142ddf01b457f0f5d
Alexandr-Dolgov <samara-fv@yandex.ru>
2015-04-23 21:27:25 +0400

    Добавил класс описания данных предметной области

---
+++ app/src/main/java/com/dolgov/accountancy/Record.java
@@ -0,0 +1,17 @@
+package com.dolgov.accountancy;
+
+import java.util.Date;
+
+/**
+ * Created by Alexandr on 23.04.2015.
+ */
+public class Record {
+    Date date;
+    double receipt;     //приход
+    double prepared;    //приготовила
+    double remainder;   //остаток
+    double sold;        //продала
+    double writeOff;    //хоз. нужды
+    double product;
+    double money;
+}
