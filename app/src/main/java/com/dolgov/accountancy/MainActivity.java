263126e6a3ffb0dfd79a3218896c2edfe53f7c67
Alexandr-Dolgov <samara-fv@yandex.ru>
2015-04-23 22:30:32 +0400

    Добавил метод очистки полей ввода. Очищаю поля ввода при переключении на новый день.

--- app/src/main/java/com/dolgov/accountancy/MainActivity.java
+++ app/src/main/java/com/dolgov/accountancy/MainActivity.java
@@ -6,12 +6,23 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
 
     TextView date;
+
+    EditText receipt;     //приход
+    EditText prepared;    //приготовила
+    EditText remainder;   //остаток
+    EditText sold;        //продала
+    EditText writeOff;    //хоз. нужды
+
+    TextView product;
+    TextView money;
+
     Button prev;
     Button next;
     Button report;
@@ -21,10 +32,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        date = (TextView) findViewById(R.id.textViewDate);
-        prev = (Button) findViewById(R.id.buttonPrev);
-        next = (Button) findViewById(R.id.buttonNext);
-        report = (Button) findViewById(R.id.buttonReport);
+        findMyViews();
 
         date.setText("0");
 
@@ -47,6 +55,8 @@ public class MainActivity extends Activity {
                 val++;
                 text = String.valueOf(val);
                 date.setText(text);
+
+                clearEditTexts();
             }
         });
 
@@ -83,5 +93,30 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void clearEditTexts(){
+        receipt.setText("");
+        prepared.setText("");
+        remainder.setText("");
+        sold.setText("");
+        writeOff.setText("");
+    }
+
+    protected void findMyViews(){
+        date = (TextView) findViewById(R.id.textViewDate);
+
+        receipt = (EditText) findViewById(R.id.editTextReceipt);
+        prepared = (EditText) findViewById(R.id.editTextPrepared);
+        remainder = (EditText) findViewById(R.id.editTextRemainder);
+        sold = (EditText) findViewById(R.id.editTextSold);
+        writeOff = (EditText) findViewById(R.id.editTextWriteOff);
+
+        product = (TextView) findViewById(R.id.textViewProduct);
+        money = (TextView) findViewById(R.id.textViewMoney);
+
+        prev = (Button) findViewById(R.id.buttonPrev);
+        next = (Button) findViewById(R.id.buttonNext);
+        report = (Button) findViewById(R.id.buttonReport);
     }
 }
