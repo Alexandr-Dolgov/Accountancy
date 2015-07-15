c956147fb562bc91d06b9cb84f9cab278cd2f736
Alexandr-Dolgov <samara-fv@yandex.ru>
2015-07-15 16:28:24 +0500

    добавлены методы возвращающие русское имя месяца по его номеру, полученному с помощью calendar.get(Calendar.MONTH), который в случае Января возвращает 0

--- app/src/main/java/com/dolgov/accountancy/Util.java
+++ app/src/main/java/com/dolgov/accountancy/Util.java
@@ -13,4 +13,47 @@ public class Util {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
         return sdf.format(date);
     }
+
+    public static String month(int numMonth){
+        String monthString;
+        switch (numMonth) {
+            case 0:  monthString = "Январь";
+                break;
+            case 1:  monthString = "Февраль";
+                break;
+            case 2:  monthString = "Март";
+                break;
+            case 3:  monthString = "Апрель";
+                break;
+            case 4:  monthString = "Май";
+                break;
+            case 5:  monthString = "Июнь";
+                break;
+            case 6:  monthString = "Июль";
+                break;
+            case 7:  monthString = "Август";
+                break;
+            case 8:  monthString = "Сентябрь";
+                break;
+            case 9:  monthString = "Октябрь";
+                break;
+            case 10: monthString = "Ноябрь";
+                break;
+            case 11: monthString = "Декабрь";
+                break;
+            default: monthString = "Invalid month";
+                break;
+        }
+        return monthString;
+    }
+
+    public static String prevMonth(int numThisMonth){
+        int numPrevMonth;
+        if (numThisMonth == 0){
+            numPrevMonth = 11;
+        } else {
+            numPrevMonth = numThisMonth - 1;
+        }
+        return month(numPrevMonth);
+    }
 }
