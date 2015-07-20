e8761d22bdf539e530f7f979e898b737c3f6fe8b
Alexandr-Dolgov <samara-fv@yandex.ru>
2015-07-20 13:32:31 +0500

    Добавил метод получения списочного массива записей по полученному ответу на SQL запрос. Изменил метод создания xls отчета, вставив вместо заглушки функциональную реализацию.

--- app/src/main/java/com/dolgov/accountancy/DatabaseAdapter.java
+++ app/src/main/java/com/dolgov/accountancy/DatabaseAdapter.java
@@ -8,8 +8,12 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.formula.functions.Column;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -19,6 +23,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -183,16 +188,117 @@ public class DatabaseAdapter {
         cursor.moveToFirst();
     }
 
-    public File createXLS(int numMonth) {
+    private ArrayList<Record> getRecords(Cursor cursor){
+        ArrayList<Record> arrayList = new ArrayList<>();
+        cursor.moveToFirst();
+        do {
+            Date date =  new Date(cursor.getLong(cursor.getColumnIndex(DATE_COLUMN)));
+            double receipt = cursor.getDouble(cursor.getColumnIndex(RECEIPT_COLUMN));
+            double prepared = cursor.getDouble(cursor.getColumnIndex(PREPARED_COLUMN));
+            double remainder = cursor.getDouble(cursor.getColumnIndex(REMAINDER_COLUMN));
+            double sold = cursor.getDouble(cursor.getColumnIndex(SOLD_COLUMN));
+            double writeOff = cursor.getDouble(cursor.getColumnIndex(WRITE_OFF_COLUMN));
+            double product = cursor.getDouble(cursor.getColumnIndex(PRODUCT_COLUMN));
+            double money = cursor.getDouble(cursor.getColumnIndex(MONEY_COLUMN));
+
+            Record record = new Record(
+                    date,
+                    receipt,
+                    prepared,
+                    remainder,
+                    sold,
+                    writeOff,
+                    product,
+                    money
+            );
+
+            arrayList.add(record);
+
+        } while (cursor.moveToNext());
+        cursor.moveToFirst();
+
+        return arrayList;
+    }
+
+    public File createXLS(int numMonth, int year) {
         Workbook wb = new HSSFWorkbook();
         Sheet sheet = wb.createSheet();
-        Row row = sheet.createRow(0);
-        Cell cell = row.createCell(0);
-        cell.setCellValue(numMonth);
+
+        int rowNum = 0;
+        Row rowOfHeaders = sheet.createRow(rowNum);
+        Cell[] cellsHeaders = new Cell[8];
+        for (int i = 0; i < cellsHeaders.length; i++) {
+            cellsHeaders[i] = rowOfHeaders.createCell(i);
+        }
+        cellsHeaders[0].setCellValue("дата");
+        cellsHeaders[1].setCellValue("приход");
+        cellsHeaders[2].setCellValue("приготовила");
+        cellsHeaders[3].setCellValue("остаток");
+        cellsHeaders[4].setCellValue("продала");
+        cellsHeaders[5].setCellValue("хоз. нужды");
+        cellsHeaders[6].setCellValue("продукты");
+        cellsHeaders[7].setCellValue("деньги");
+        //задаем границы для ечеек с загаловками
+        for (int i = 0; i < cellsHeaders.length; i++) {
+            CellStyle style = wb.createCellStyle();
+            style.setBorderTop(CellStyle.BORDER_THIN);
+            style.setBorderRight(CellStyle.BORDER_THIN);
+            style.setBorderBottom(CellStyle.BORDER_THIN);
+            style.setBorderLeft(CellStyle.BORDER_THIN);
+            cellsHeaders[i].setCellStyle(style);
+        }
+
+        //находим все записи у которых год и месяц соответствую указанным
+        long beginMonth = Util.unixTimeBeginMonth(year, numMonth);
+        long endMonth = Util.unixTimeEndMonth(year, numMonth);
+        String sql = "SELECT * " +
+                " FROM " + TABLE_NAME +
+                " WHERE (date >= " + beginMonth + ") AND (date <= " + endMonth + ");";
+        Log.d(TAG, "sql = " + sql);
+        Cursor cursor = database.rawQuery(sql, null);
+        ArrayList<Record> records = getRecords(cursor);
+        for (Record record : records) {
+            rowNum++;
+            Row row = sheet.createRow(rowNum);
+            Cell[] cells = new Cell[8];
+            for (int i = 0; i < cells.length; i++) {
+                cells[i] = row.createCell(i);
+            }
+            cells[0].setCellValue(Util.unixTimeToString(record.getDate().getTime()));
+            cells[1].setCellValue(record.getReceipt());
+            cells[2].setCellValue(record.getPrepared());
+            cells[3].setCellValue(record.getRemainder());
+            cells[4].setCellValue(record.getSold());
+            cells[5].setCellValue(record.getWriteOff());
+            cells[6].setCellValue(record.getProduct());
+            cells[7].setCellValue(record.getMoney());
+            //задаем границы как прямые тонкие линии
+            for (int i = 0; i < cells.length; i++) {
+                CellStyle style = wb.createCellStyle();
+                style.setBorderTop(CellStyle.BORDER_THIN);
+                style.setBorderRight(CellStyle.BORDER_THIN);
+                style.setBorderBottom(CellStyle.BORDER_THIN);
+                style.setBorderLeft(CellStyle.BORDER_THIN);
+                cells[i].setCellStyle(style);
+            }
+            //устанавливаем формат ячеек как чило с двумя знаками после десятичного разделителя
+            for (int i = 1; i < cells.length; i++) {
+                CellStyle style = cells[i].getCellStyle();
+                short format = wb.createDataFormat().getFormat("0.00");
+                style.setDataFormat(format);
+                cells[i].setCellStyle(style);
+            }
+        }
+
+        //устанавливаем ширину для столбцов
+        for (int i = 0; i < 8; i++) {
+            sheet.setColumnWidth(i, 2634);
+        }
+        sheet.setColumnWidth(2, 2816);
 
         // Write the output to a file
         String dirPath = activity.getApplicationInfo().dataDir;
-        String fileName = Util.monthToString(numMonth) + ".xls";
+        String fileName = Util.monthToString(numMonth) + year + ".xls";
         File file = new File(dirPath, fileName);
         Log.d(TAG, "xlsFile AbsolutePath = " + file.getAbsolutePath());
         try {
